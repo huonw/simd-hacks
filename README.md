dd2d6fb0620d31ef067f90d2ca49e8d1d9f6c47a
Huon Wilson <dbau.pp+github@gmail.com>
2015-01-23 16:53:56 +1100

    Tweak the benchmark slightly: sqrts and f32s

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Functionality for working with SIMD registers and instructions.
+
+`benches/sum-vector.rs`:
+
+```
+running 2 tests
+test nonsimd ... bench:      2569 ns/iter (+/- 122)
+test simd_   ... bench:       611 ns/iter (+/- 23)
+
+test result: ok. 0 passed; 0 failed; 0 ignored; 2 measured
+```
--- benches/sum-vector.rs
+++ benches/sum-vector.rs
@@ -7,23 +7,23 @@ use simd::*;
 
 #[bench]
 fn simd_(b: &mut test::Bencher) {
-    let v = (0..10000).map(|i| i as f64).collect::<Vec<_>>();
+    let v = (0..1000).map(|i| i as f32).collect::<Vec<_>>();
 
-    // sum a f64 vector as i32's, via SIMD.
+    // sum the roots of an f32 vector as i32's, via SIMD.
     b.iter(|| {
-        let Tails {start,middle,end}: Tails<_, f64x2> = test::black_box(&v[]).convert();
+        let Tails {start,middle,end}: Tails<_, f32x4> = test::black_box(&v[]).convert();
         let mut ret = i32x4(0, 0, 0, 0);
         for elem in middle.iter() {
-            ret = ret + elem.convert();
+            ret = ret + sqrt(*elem).convert();
         }
-        let leftovers = start.iter().chain(end.iter()).fold(0, |a, b| a + *b as i32);
+        let leftovers = start.iter().chain(end.iter()).fold(0, |a, b| a + sqrt(*b) as i32);
 
-        ret.0 + ret.1 + leftovers
+        ret.0 + ret.1 + ret.2 + ret.3 + leftovers
     })
 }
 #[bench]
 fn nonsimd(b: &mut test::Bencher) {
-    let v = (0..10000).map(|i| i as f64).collect::<Vec<f64>>();
+    let v = (0..1000).map(|i| i as f32).collect::<Vec<f32>>();
 
-    b.iter(|| test::black_box(&v[]).iter().fold(0, |a, b| a + *b as i32))
+    b.iter(|| test::black_box(&v[]).iter().fold(0, |a, b| a + sqrt(*b) as i32))
 }
